03a08d8e308dd8012eb82e1249eca8fda4685580
Sabri <sabri.abbassidoukkali@gmail.com>
2022-02-07 15:31:29 +0100

    Create and Edit a Category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CategoryController extends AbstractController
 {
@@ -26,14 +32,57 @@ class CategoryController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/admin/category/create", name="category_create")
+     */
+    public function create(Request $request, EntityManagerInterface $em, SluggerInterface $slug)
+    {
+        $category = new Category;
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $category->setSlug(strtolower($slug->slug($category->getName())));
+
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute("homepage");
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('category/create.html.twig', [
+            'formView' => $formView
+        ]);
+    }
 
     /**
-     * @Route("/category", name="category")
+     * @Route("/admin/category/{id}/edit", name="category_edit")
      */
-    public function index(): Response
+    public function edit($id, CategoryRepository $categoryRepository, Request $request, EntityManagerInterface $em, SluggerInterface $slug)
     {
-        return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+        $category = $categoryRepository->find($id);
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+
+            $category->setSlug(strtolower($slug->slug($category->getName())));
+            $em->flush();
+
+            return $this->redirectToRoute("homepage");
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
+            'formView' => $formView
         ]);
     }
 }
